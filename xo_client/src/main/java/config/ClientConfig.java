b896f06dbca57a53b6baeed4b264e968c5069b19
dmrval <hal9van1@gmail.ru>
2020-06-21 17:50:20 +0400

    [master] first init

---
+++ xo_client/src/main/java/config/ClientConfig.java
@@ -0,0 +1,6 @@
+package config;
+
+public interface ClientConfig {
+    int BUFFER_SIZE = 4096;
+    int GAME_PORT = 7000;
+}
---
+++ xo_server/src/main/java/config/ServerConfig.java
@@ -0,0 +1,6 @@
+package config;
+
+public interface ServerConfig {
+    int BUFFER_SIZE = 4096;
+    int GAME_PORT = 7000;
+}
