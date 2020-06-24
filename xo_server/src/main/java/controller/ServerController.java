23cb5e6c2ee4db7f34f315d2fe89bf89275972a3
DamirValeev <dmr.val@mail.ru>
2020-06-24 17:50:45 +0400

    [] add 9/9 part cell

--- xo_server/src/main/java/controller/ServerController.java
+++ xo_server/src/main/java/controller/ServerController.java
@@ -23,18 +23,23 @@ public class ServerController {
     Button cell01;
     @FXML
     Button cell02;
+    @FXML
+    Button cell10;
+    @FXML
+    Button cell11;
+    @FXML
+    Button cell12;
+    @FXML
+    Button cell20;
+    @FXML
+    Button cell21;
+    @FXML
+    Button cell22;
 
     public ServerController() {
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         xoServer = new XOServer();
         executorService.execute(xoServer);
-    }
-
-    public void setField00(MouseEvent mouseEvent) {
-        if (yourStep()) {
-            cell00.setText("X");
-            xoServer.getGameBoard().getCells()[0][0].setStatus(Status.X);
-        }
     }
 
     private boolean yourStep() {
@@ -44,24 +49,77 @@ public class ServerController {
                 log.info("НЕ ТВОЙ ХОД ШАКАЛ");
             }
             return xoServer.getGameBoard().getWhoseMove() == WhoseMove.SERVER;
-        }
-        else {
+        } else {
             return false;
+        }
+    }
+
+    private void responseStep() {
+        xoServer.getGameBoard().getCells()[0][1].setStatus(Status.X);
+        xoServer.getGameBoard().setWhoseMove(WhoseMove.CLIENT);
+    }
+
+
+    public void setField00(MouseEvent mouseEvent) {
+        if (yourStep()) {
+            cell00.setText("X");
+            responseStep();
         }
     }
 
     public void setField01(MouseEvent mouseEvent) {
         if (yourStep()) {
             cell01.setText("X");
-            xoServer.getGameBoard().getCells()[0][1].setStatus(Status.X);
+            responseStep();
         }
     }
 
     public void setField02(MouseEvent mouseEvent) {
         if (yourStep()) {
             cell02.setText("X");
-            xoServer.getGameBoard().getCells()[0][2].setStatus(Status.X);
+            responseStep();
         }
     }
 
+    public void setField10(MouseEvent mouseEvent) {
+        if (yourStep()) {
+            cell10.setText("X");
+            responseStep();
+        }
+    }
+
+    public void setField11(MouseEvent mouseEvent) {
+        if (yourStep()) {
+            cell11.setText("X");
+            responseStep();
+        }
+    }
+
+    public void setField12(MouseEvent mouseEvent) {
+        if (yourStep()) {
+            cell12.setText("X");
+            responseStep();
+        }
+    }
+
+    public void setField20(MouseEvent mouseEvent) {
+        if (yourStep()) {
+            cell20.setText("X");
+            responseStep();
+        }
+    }
+
+    public void setField21(MouseEvent mouseEvent) {
+        if (yourStep()) {
+            cell21.setText("X");
+            responseStep();
+        }
+    }
+
+    public void setField22(MouseEvent mouseEvent) {
+        if (yourStep()) {
+            cell22.setText("X");
+            responseStep();
+        }
+    }
 }
